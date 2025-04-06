f89225d8b51ebdb066559d2d4adfdffee6f5abb2
Siddu <sidduswamy925@gmail.com>
2025-04-06 21:50:36 +0530

    updated

--- Zippy/src/main/java/in/shraddha/repository/UserRepo.java
+++ Zippy/src/main/java/in/shraddha/repository/UserRepo.java
@@ -7,8 +7,11 @@ import org.springframework.data.jpa.repository.Query;
 import in.shraddha.entity.User;
 
 public interface UserRepo extends JpaRepository<User, Integer>{
+	
+	
 	User findByEmail(String email);
 
+	//@Query("select u from User u where u.email=:email")
 	boolean existsByEmail(String email); 
 	
 	
