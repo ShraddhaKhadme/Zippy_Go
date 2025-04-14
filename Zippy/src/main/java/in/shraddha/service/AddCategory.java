9f1aec7aafab442ebd7101d54c251c87e14f8538
Siddu <sidduswamy925@gmail.com>
2025-04-14 19:56:43 +0530

    updated

--- Zippy/src/main/java/in/shraddha/service/AddCategory.java
+++ Zippy/src/main/java/in/shraddha/service/AddCategory.java
@@ -5,6 +5,7 @@ import java.util.List;
 import in.shraddha.entity.Category;
 
 public interface AddCategory {
+	
 	public void addCategory(String categoryName, String image, String subcategoryName);
 
 	public List<Category> getAllCategories();
